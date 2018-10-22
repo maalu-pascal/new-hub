3818572d2ca2f08449434c908d8f19d3ed417e01
maalu-pascal <maalupascal@gmail.com>
2018-10-22 19:36:33 +0530

    Dropdown for the branch button and clone button created. Readme div created.

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ function changeSummary() {
         element.classList.remove('summary-wrapper-switch') :
         element.classList.add('summary-wrapper-switch');
 }
+
+function filterFunction() {
+    let input, filter, spanDiv, increment;
+    input = document.getElementById("search-branch-tag");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("dropdown-content");
+    spanDiv = div.getElementsByTagName("span");
+    for (increment = 0; increment < spanDiv.length; increment++) {
+        if (spanDiv[increment].innerHTML.toUpperCase().indexOf(filter) > -1) {            
+            spanDiv[increment].style.display = "block";
+        } else {
+            spanDiv[increment].style.display = "none";
+        }
+    }
+}
